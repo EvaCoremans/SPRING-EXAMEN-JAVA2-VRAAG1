2c76ad7b1c92b40b0fd4867c64d7f14ed29d8e4c
EvaCoremans <e.coremans@gmail.com>
2018-09-07 08:58:41 +0200

    Add repository to github, created question entity

---
+++ src/main/java/edu/ap/spring/jpa/Question.java
@@ -0,0 +1,51 @@
+package edu.ap.spring.jpa;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Question {
+    @Id
+    @GeneratedValue
+    Long id;
+
+    @Column
+    String question;
+
+    @Column
+    String answer;
+
+    public Question() {
+    }
+
+    //region Access Methods
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getQuestion() {
+        return question;
+    }
+
+    public void setQuestion(String question) {
+        this.question = question;
+    }
+
+    public String getAnswer() {
+        return answer;
+    }
+
+    public void setAnswer(String answer) {
+        this.answer = answer;
+    }
+    //endregion
+
+
+
+}
